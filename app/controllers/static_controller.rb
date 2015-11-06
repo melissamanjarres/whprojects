3e46aff33aec8bc7647c90d8a33197bab0c8c3f3
melissamanjarres <melissaam@uninorte.edu.co>
2015-11-06 07:35:56 +0000

    Styling

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+    def aboutus
+    end
+    def projects
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +57,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get 'aboutus' => 'static#about-us', as: :about
+  get 'projects' => 'static#projects', as: :projects
 end

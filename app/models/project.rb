114495bfc04e4b4751b1cc4fe8d1d7371e01d4c0
Andrea Zapata <andreaczapata@gmail.com>
2015-11-11 17:44:06 -0500

    Bugs fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
     belongs_to :user
     validates :user_id, presence: true
+    validates :name, presence: true
     
     has_attached_file :avatar_project, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "project-default.jpg"
     validates_attachment_content_type :avatar_project, content_type: /\Aimage\/.*\Z/
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ActiveRecord::Base
   has_many :teamrelationships, dependent: :destroy
   has_many :users, through: :teamrelationships
   validates :user_id, presence: true
-
+  validates :name, presence: true
   def self.search(query)
     	where("name like ?","#{query}%")
   end	

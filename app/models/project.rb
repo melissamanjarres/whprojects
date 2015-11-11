d3837b5b482750157bc8ecbdb857bb9c5ec5820d
Andrea Zapata <andreaczapata@gmail.com>
2015-11-10 22:00:57 -0500

    Change default images+ new project & team btn

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
     belongs_to :user
     validates :user_id, presence: true
     
-    has_attached_file :avatar_project, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar_project, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "project-default.jpg"
     validates_attachment_content_type :avatar_project, content_type: /\Aimage\/.*\Z/
     
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,7 @@ class User < ActiveRecord::Base
   has_many :teamrelationships, dependent: :destroy
   has_many :teams, through: :teamrelationships
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "default-avatar.png"
 
   def team_member?(team)
     teamrelationships.find_by_team_id(team.id)
